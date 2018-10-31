3d3a55557618c2a01283724a854cd45a97319d0a
Erin Gruenwedel <Erin.Gruenwedel@iam.local>
2018-10-31 14:26:52 -0500

    Added jumping code

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -7,7 +7,15 @@ public class CharacterControllerScript : MonoBehaviour
     public float maxSpeed = 10;
     bool facingRight = true;
     private Rigidbody2D rigidbody2D;
+
     Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -18,6 +26,11 @@ public class CharacterControllerScript : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
 
         anim.SetFloat("Speed", Mathf.Abs(move));
@@ -29,6 +42,15 @@ public class CharacterControllerScript : MonoBehaviour
         else if (move < 0 && facingRight)
             Flip();
 	}
+
+    void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
 
     void Flip()
     {
