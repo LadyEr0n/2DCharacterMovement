25329147c28e8c915314ea81cccc192a0b119ba3
Erin Gruenwedel <Erin.Gruenwedel@iam.local>
2018-10-31 13:59:43 -0500

    Attached running and idle animations to character

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    bool facingRight = true;
+    private Rigidbody2D rigidbody2D;
+    Animator anim;
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
